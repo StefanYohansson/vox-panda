147538bd0f12a7137b5c98e1d47056d23a6b4201
netoguimaraes <joao.neto.guimaraes@gmail.com>
2017-12-26 22:45:35 -0300

    adding validation in passwords

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,12 +3,18 @@ import { Link } from 'react-router-dom';
 import { LocalForm, Control } from 'react-redux-form';
 import Logo from 'vox/components/Logo';
 
+const emptyErrors = {
+  username: [],
+  password: [],
+  email: [],
+}
+
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       registerForm: false,
-      errors: []
+      errors: { ...emptyErrors }
     }
   }
 
@@ -16,18 +22,26 @@ class Login extends Component {
     this.formDispatch = formDispatch;
   }
 
-  handleSubmit = values => {
-    console.log(values, this.state.registerForm)
-  }
-
   handleChange = values => {
-    console.log('handleChange', values)
+    this.setState({...this.state, errors: { ...emptyErrors }}, () => {
+      const state = this.state;
+      if(state.registerForm && values.password != values.confirmPassword)
+        this.setState({
+          ...state,
+          errors: {
+            ...state.errors,
+            password: new Array("Passwords doesn't match")
+          }
+        })
+    })
   }
 
   render() {
-    const { registerForm } = this.state;
+    const { registerForm, errors } = this.state;
     const auxLink = registerForm ? 'Already have an account? Log here' : 'Register';
     const buttonMessage = registerForm ? 'Register' : 'Login'; 
+    const hasErrors = Object.keys(errors).reduce((acc, key) => acc + errors[key].length, 0) > 0;
+    const allErrors = Object.keys(errors).reduce((acc, key) => acc.concat(errors[key]), []);
     return (
       <div className="login-container">
         <Logo />
@@ -64,7 +78,7 @@ class Login extends Component {
                 <fieldset className="form-group">
                   <label htmlFor="confirm-password">confirm password:</label>
                   <Control.password
-                    model=".confirm-password"
+                    model=".confirmPassword"
                     id="confirm-password"
                     required
                     type="password"
@@ -85,6 +99,11 @@ class Login extends Component {
              )
           :null}
           <div className="form-actions">
+            {
+              hasErrors ?
+              <div className="alert alert-error">{allErrors.map(item => <p>{item}</p>)}</div>
+              : null
+            }
             <button type="submit" className="btn btn-primary btn-block">{buttonMessage}</button>
             <a className="aux-link" onClick={() => this.setState({...this.state, registerForm: !registerForm})}>{auxLink}</a>
           </div>
